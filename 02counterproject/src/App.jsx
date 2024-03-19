25f67643480b9783110ac4ea432328f67a247a70
ChinmayeeSabar <chinmayeechinu02@gmail.com>
2024-03-19 08:53:10 +0530

    add the files

--- 02counterproject/src/App.jsx
+++ 02counterproject/src/App.jsx
@@ -10,11 +10,13 @@ function App() {
 
   const addValue = () =>{
     if(counter < 20)
-    // setCounter(counter + 1) //To increament by 1
+    //  setCounter(counter + 1) //To increament by 1
+  
   setCounter(prevCounter => prevCounter + 1)
   setCounter(prevCounter => prevCounter + 1)
   setCounter(prevCounter => prevCounter + 1)
   setCounter(prevCounter => prevCounter + 1)
+  
   }
   
   const removeValue = () =>{
